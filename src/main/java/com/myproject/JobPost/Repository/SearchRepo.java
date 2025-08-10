aadfa9c76883334b72280b608672a83a10ca857c
ramesh goud <rgoud1739@gmail.com>
2025-08-10 23:28:58 +0530

    Added Docker File

--- src/main/java/com/myproject/JobPost/Repository/SearchRepo.java
+++ src/main/java/com/myproject/JobPost/Repository/SearchRepo.java
@@ -1,9 +1,10 @@
 package com.myproject.JobPost.Repository;
 
 import com.myproject.JobPost.model.Post;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface SearchRepo {
     List<Post> searchpost(String text);
 }
